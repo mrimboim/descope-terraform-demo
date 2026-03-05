0163d17e2c011e139bde720d414a659e0aa65bbc
Michael Rimboim <81452346+mrimboim@users.noreply.github.com>
2026-03-05 09:32:25 -0800

    Enhance main.tf with new settings and configurations
    Added project settings, authentication, attributes, and authorization configurations.

--- main.tf
+++ main.tf
@@ -19,6 +19,65 @@ resource "descope_project" "project" {
     }
   }
 
+  tags = ["foo", "bar"]
+ 
+  project_settings = {
+    refresh_token_expiration = "3 weeks"
+    enable_inactivity = true
+    inactivity_time = "2 hour"
+  }
+ 
+  authentication = {
+    magic_link = {
+      expiration_time = 3600
+      expiration_time_unit "seconds"
+    }
+    password = {
+        lock = true
+        lock_attempts = 3
+        min_length = 8
+    }
+    sso = {
+        merge_users = true
+        redirect_url = https://apple.com
+    }
+  }
+ 
+  attributes = {
+    user = [ {
+      name = "test attribute user"
+      type = "string"
+    } ]
+    tenant = [ {
+      name = "test attribute tenant"
+      type = "multiselect"
+      select_options = ["A", "B"]
+    } ]
+  }
+ 
+  authorization = {
+    permissions = [ {
+      name = "test-permission"
+      description = "this is a test"
+    } ]
+    roles = [{
+      name = "test-role"
+      description = "this is a test"
+      permissions = ["test-permission"]
+    }]
+  }
+ 
+    applications = {
+      oidc_applications = [ {
+        name = "test OIDC app"
+        description = "This is a test"
+        force_authentication = false
+        claims = ["sub", "exp"]
+      } ]
+    }
+ 
+ 
+
   connectors = {
     "hibp": [
       {
